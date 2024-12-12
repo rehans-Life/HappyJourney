6f3242291e7c8935c3cb6d8b99765b8f278d3c4e
Zainab <202201685@student.polytechnic.bh>
2024-12-12 21:48:11 +0300

    Commit

--- HappyJourney/Form1.Designer.cs
+++ HappyJourney/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.btnBookFlight = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.Location = new System.Drawing.Point(358, 77);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(122, 53);
+            this.btnSignup.TabIndex = 0;
+            this.btnSignup.Text = "Sign up";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            this.btnSignup.Click += new System.EventHandler(this.btnSignup_Click);
+            // 
+            // btnBookFlight
+            // 
+            this.btnBookFlight.Location = new System.Drawing.Point(344, 164);
+            this.btnBookFlight.Name = "btnBookFlight";
+            this.btnBookFlight.Size = new System.Drawing.Size(157, 56);
+            this.btnBookFlight.TabIndex = 1;
+            this.btnBookFlight.Text = "Book flight";
+            this.btnBookFlight.UseVisualStyleBackColor = true;
+            this.btnBookFlight.Click += new System.EventHandler(this.btnBookFlight_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dashboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBookFlight);
+            this.Controls.Add(this.btnSignup);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.Button btnBookFlight;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HappyJourney/Form1.cs
+++ HappyJourney/Form1.cs
@@ -16,5 +16,26 @@ namespace HappyJourney
         {
             InitializeComponent();
         }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            Signup signupForm = new Signup();
+            signupForm.Show();
+            this.Hide();
+        }
+
+        private void btnBookFlight_Click(object sender, EventArgs e)
+        {
+            Book_flight book_Flight = new Book_flight();
+            book_Flight.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            this.Hide();
+        }
     }
 }
